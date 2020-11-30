2c5e15e62d70d8a7becae4a10ee46848e56c79cf
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-11-30 17:10:17 -0600

    Changed Cards variable to a dictionary instead of a list

--- BlackJack/Game.py
+++ BlackJack/Game.py
@@ -8,20 +8,19 @@ the player and allow them to place bets
 """
 import random
 MAX = 21
-CARDS = ['HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
-         'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
-         'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
-         'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK']
+CARDS = {1: 'HA', 2: 'H2', 3: 'H3', 4: 'H4', 5: 'H5', 6: 'H6', 7: 'H7', 8: 'H8', 9: 'H9', 10: 'H10', 11: 'HJ', 12: 'HQ',
+         13: 'HK',
+         14: 'CA', 15: 'C2', 16: 'C3', 17: 'C4', 18: 'C5', 19: 'C6', 20: 'C7', 21: 'C8', 22: 'C9', 23: 'C10', 24: 'CJ',
+         25: 'CQ', 26: 'CK',
+         27: 'DA', 28: 'D2', 29: 'D3', 30: 'D4', 31: 'D5', 32: 'D6', 33: 'D7', 34: 'D8', 35: 'D9', 36: 'D10', 37: 'DJ',
+         38: 'DQ', 39: 'DK',
+         40: 'SA', 41: 'S2', 42: 'S3', 43: 'S4', 44: 'S5', 45: 'S6', 46: 'S7', 47: 'S8', 48: 'S9', 49: 'S10', 50: 'SJ',
+         51: 'SQ', 52: 'SK'}
+
 class Game:
     def __init__(self, dealer_t, player_t):
         self.dealer_total = dealer_t
         self.player_total = player_t
 
     def game(self):
-        CARDS.sort(key=self.shuffle)
-
-        print(self.player_total)
-
-    def shuffle(self, n):
-        i = random.randint(1, 53)
-        return abs(n - i)
+        pass
